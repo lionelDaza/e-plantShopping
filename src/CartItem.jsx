a45acd2b5cfb6c10ee3af250537c13e578c1c01a
Alvaro Daza Quiroga <Alvaro.Daza@jalasoft.com>
2024-08-07 20:40:27 -0400

    Second commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,33 +3,42 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
+const CartItem = ({ onContinueShopping, setAddedToCart }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => total + (item.quantity * parseInt(item.cost.substring(1))), 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ ...item, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ ...item, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item))
+    setAddedToCart(prevState => ({
+        ...prevState,
+        [item.name]: false,
+    }));
+  };
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    var totalByItem = item.quantity * parseInt(item.cost.substring(1));
+    return totalByItem
   };
 
   return (
@@ -57,7 +66,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,17 +8,15 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const dispatch = useDispatch()
     const cart = useSelector(state => state.cart.items);
+    const totalItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
     const [addToCart, setAddedToCart] = useState({});
     useEffect(() => {
-
         const initialAddedToCart = {};
         cart.forEach(item => {
-          initialAddedToCart[item.name] = true; // Assuming item.name is unique identifier
+          initialAddedToCart[item.name] = true; 
         });
         setAddedToCart(initialAddedToCart);
     }, [cart])
-
-
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -260,8 +258,7 @@ const isProductAdded = (productName) => {
    };
 
    const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
+    setShowCart(false)
   };
 
   const handleAddToCart = (product) => {
@@ -288,7 +285,10 @@ const isProductAdded = (productName) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                </svg>
+                {totalItemsInCart > 0 && (<span className="cart-badge">{totalItemsInCart}</span>)}</h1></a>
+                </div>
             </div>
         </div>
         {!showCart? (
